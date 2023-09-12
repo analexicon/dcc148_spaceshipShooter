ca5e8ffb1f12524b31c5f4bb77178da52184a38c
Lucas Paiva <lucaspaiva@live.com>
2023-09-12 17:55:08 -0300

    adiciona criação de straightEnemy com object pool

--- Assets/Scripts/enemySpawnerController.cs
+++ Assets/Scripts/enemySpawnerController.cs
@@ -7,17 +7,26 @@ public class enemySpawnerController : MonoBehaviour
     public float speed = 2;
     private float direction;
     public ObjectPool straightEnemyPool;
+    public int straightEnemyAmount = 5;
     public GameObject straightEnemyPrefab;
     public ObjectPool sinusoidalEnemyPool;
     public ObjectPool sinusoidalEnemyPrefab;
     public ObjectPool aproachingEnemyPool;
     public ObjectPool aproachingEnemyPrefab;
 
+    public float minInterval = 0.2f;
+    public float maxInterval = 10000f;
+
+    private float nextSpawnTime;
 
     // Start is called before the first frame update
     void Start()
     {
         direction = 1;
+        straightEnemyPool = new ObjectPool(straightEnemyPrefab, straightEnemyAmount);
+        nextSpawnTime = Time.time + Random.Range(minInterval, maxInterval);
+        straightEnemySpawn();
+
     }
 
     // Update is called once per frame
@@ -38,5 +47,24 @@ public class enemySpawnerController : MonoBehaviour
         }
 
         transform.position = pos;
+
+
+
+        // if (Time.time >= nextSpawnTime)
+        // straightEnemySpawn();
     }
+
+    void straightEnemySpawn()
+    {
+        GameObject straightEnemy = straightEnemyPool.GetFromPool();
+
+        if (straightEnemy != null)
+        {
+            Vector2 pos = transform.position;
+            straightEnemy.transform.position = pos;
+            straightEnemy.SetActive(true);
+
+        }
+    }
+
 }
--- Assets/Scripts/straightEnemyController.cs
+++ Assets/Scripts/straightEnemyController.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 public class straightEnemyController : MonoBehaviour
 {
     public float speed = 3;
+    public float initialX;
+    public float initialY;
+
+    private Vector2 posUpdater; // Alterei para uma variável de classe
+
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject enemySpawner = GameObject.Find("EnemySpawner");
+        Debug.Log(enemySpawner.transform.position.x);
+        if (enemySpawner != null)
+        {
+            initialY = enemySpawner.transform.position.y;
+            initialX = enemySpawner.transform.position.x;
+            // Atualize a variável de classe 'posUpdater' em vez de redeclará-la
+            posUpdater = new Vector2(initialX, initialY);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
+        posUpdater.x -= speed * Time.deltaTime;
+        posUpdater.y = initialY;
+        transform.position = posUpdater;
 
-        pos.x -= speed * Time.deltaTime;
-
-        transform.position = pos;
-
-        Vector2 minPos = new Vector2(-8, 0);
-        if (transform.position.x < minPos.x)
+        float minPos = -8;
+        if (transform.position.x < minPos)
             gameObject.SetActive(false);
     }
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         gameObject.SetActive(false);
