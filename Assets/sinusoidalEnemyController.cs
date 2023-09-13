fdc5c580e17637ec7b2a4c04b71dcd905c861f66
Lucas Paiva <lucaspaiva@live.com>
2023-09-13 02:02:51 -0300

    adiciona prefab de inimigo senoidal

--- Assets/sinusoidalEnemyController.cs
+++ Assets/sinusoidalEnemyController.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class straightEnemyController : MonoBehaviour
+public class WavyEnemyController : MonoBehaviour
 {
     public float speed = 3;
-    public float initialX;
-    public float initialY;
+    public float rotationSpeed = 90.0f;
+    public float amplitude = 3.0f;
+    public float frequency = 5.0f;
 
     private Vector2 posUpdater;
+    private float initialX;
+    private float initialY;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject enemySpawner = GameObject.Find("EnemySpawner");
-        Debug.Log(enemySpawner.transform.position.x);
+
         if (enemySpawner != null)
         {
             initialY = enemySpawner.transform.position.y;
@@ -28,8 +31,11 @@ public class straightEnemyController : MonoBehaviour
     void Update()
     {
         posUpdater.x -= speed * Time.deltaTime;
-        posUpdater.y = initialY;
+        posUpdater.y = initialY + Mathf.Sin(Time.time * frequency) * amplitude;
         transform.position = posUpdater;
+
+        float rotationAmount = rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.forward, rotationAmount);
 
         float minPos = -8;
         if (transform.position.x < minPos)
